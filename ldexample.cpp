05728c31947f59fb85d7b020f50f98d4b8df3588
Szymon Scharmach <szymon.scharmach@intel.com>
2019-01-05 14:58:11 +0100

    Added shared lib for LD_PRELOAD

--- ldexample.cpp
+++ ldexample.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 int main(int argc, char **argv) {
-	cout << "Hello world";
+	cout << "Hello" << endl;
+	srand(time(NULL));
+	int max_num=10;
+	for (int i=0;i<=max_num;i++) {
+		cout << "Random number is: " << rand()%100 << endl;
+	}
 	return 0;
 }
